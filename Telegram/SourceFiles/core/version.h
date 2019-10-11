005ddd23cf0013490f9a005b0b776e727e295d5f
RadRussianRus <radrussianrus@gmail.com>
2019-10-11 07:00:40 +0300

    Beta version 1.0.9

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -17,7 +17,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 constexpr auto AppVersion = 1008015;
 constexpr auto AppVersionStr = "1.8.15";
-constexpr auto AppBetaVersion = false;
+constexpr auto AppBetaVersion = true;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
-constexpr auto AppKotatoVersion = 1000008;
-constexpr auto AppKotatoVersionStr = "1.0.8";
+constexpr auto AppKotatoVersion = 1000009;
+constexpr auto AppKotatoVersionStr = "1.0.9";
