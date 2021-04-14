f352b5bc58698fc66b2b38585d777406c99829f6
RadRussianRus <radrussianrus@gmail.com>
2021-04-14 18:02:25 +0300

    Move Kotatogram-related versioning out of TDesktop

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -7,7 +7,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #pragma once
 
-#include "base/const_string.h"
+// Already in kotato/version.h
+//#include "base/const_string.h"
 #include "kotato/version.h"
 
 #define TDESKTOP_REQUESTED_ALPHA_VERSION (0ULL)
@@ -19,11 +20,11 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #endif // TDESKTOP_ALLOW_CLOSED_ALPHA
 
 // used in Updater.cpp and Setup.iss for Windows
-constexpr auto AppId = "{C4A4AE8F-B9F7-4CC7-8A6C-BF7EEE87ACA5}"_cs;
+//constexpr auto AppId = "{53F49750-6209-4FBF-9CA8-7A333C87D1ED}"_cs;
 constexpr auto AppNameOld = "Telegram Win (Unofficial)"_cs;
-constexpr auto AppName = "Kotatogram Desktop"_cs;
-constexpr auto AppFile = "Kotatogram"_cs;
+//constexpr auto AppName = "Telegram Desktop"_cs;
+//constexpr auto AppFile = "Telegram"_cs;
 constexpr auto AppVersion = 2007001;
 constexpr auto AppVersionStr = "2.7.1";
-constexpr auto AppBetaVersion = false;
+//constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
--- Telegram/SourceFiles/kotato/version.h
+++ Telegram/SourceFiles/kotato/version.h
@@ -6,6 +6,12 @@ For license and copyright information please follow this link:
 https://github.com/kotatogram/kotatogram-desktop/blob/dev/LEGAL
 */
 #pragma once
+#include "base/const_string.h"
 
+// used in Updater.cpp and Setup.iss for Windows
+constexpr auto AppId = "{C4A4AE8F-B9F7-4CC7-8A6C-BF7EEE87ACA5}"_cs;
+constexpr auto AppName = "Kotatogram Desktop"_cs;
+constexpr auto AppFile = "Kotatogram"_cs;
 constexpr auto AppKotatoVersion = 1004000;
 constexpr auto AppKotatoVersionStr = "1.4";
+constexpr auto AppBetaVersion = false;
