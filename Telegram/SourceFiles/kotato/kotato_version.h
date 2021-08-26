38d6f337ccfbbfb43eabf185c468420fa71a2eee
RadRussianRus <radrussianrus@gmail.com>
2021-08-26 17:18:08 +0300

    Version 1.4.3 beta

--- Telegram/SourceFiles/kotato/kotato_version.h
+++ Telegram/SourceFiles/kotato/kotato_version.h
@@ -7,8 +7,8 @@ https://github.com/kotatogram/kotatogram-desktop/blob/dev/LEGAL
 */
 #pragma once
 
-constexpr auto AppKotatoVersion = 1004002;
-constexpr auto AppKotatoVersionStr = "1.4.2";
+constexpr auto AppKotatoVersion = 1004003;
+constexpr auto AppKotatoVersionStr = "1.4.3";
 constexpr auto AppKotatoBetaVersion = true;
 
 //#define KTGDESKTOP_IS_TEST_VERSION
