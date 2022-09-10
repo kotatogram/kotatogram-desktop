010681e6006ddd93ff3d78a563debb0c4786159b
RadRussianRus <radrussianrus@gmail.com>
2022-09-10 20:39:51 +0300

    [Improvement] Copy sticker pack title

--- Telegram/SourceFiles/boxes/sticker_set_box.h
+++ Telegram/SourceFiles/boxes/sticker_set_box.h
@@ -47,6 +47,7 @@ private:
 	bool showMenu(not_null<Ui::IconButton*> button);
 	void addStickers();
 	void copyStickersLink();
+	void copyTitle();
 	void handleError(Error error);
 
 	const not_null<Window::SessionController*> _controller;
