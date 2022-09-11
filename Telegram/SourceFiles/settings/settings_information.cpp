f0c81eb54c2ccb7ee7c5d7d06bc682b8e37b62e8
RadRussianRus <radrussianrus@gmail.com>
2022-09-11 06:25:04 +0300

    [Improvement] Do not hide "mark as read" under Alt+Shift modfiers

--- Telegram/SourceFiles/settings/settings_information.cpp
+++ Telegram/SourceFiles/settings/settings_information.cpp
@@ -652,16 +652,6 @@ void SetupAccountsWrap(
 		} else if (which != Qt::RightButton) {
 			return;
 		}
-		if (!state->menu && IsAltShift(raw->clickModifiers()) && !locked) {
-			state->menu = base::make_unique_q<Ui::PopupMenu>(
-				raw,
-				st::popupMenuWithIcons);
-			Window::MenuAddMarkAsReadAllChatsAction(
-				window,
-				Ui::Menu::CreateAddActionCallback(state->menu));
-			state->menu->popup(QCursor::pos());
-			return;
-		}
 		if (session == &window->session() || state->menu) {
 			return;
 		}
@@ -686,6 +676,9 @@ void SetupAccountsWrap(
 			addAction(tr::lng_menu_activate(tr::now), [=] {
 				callback({});
 			}, &st::menuIconProfile);
+			Window::MenuAddMarkAsReadAllChatsAction(
+				window,
+				Ui::Menu::CreateAddActionCallback(state->menu));
 		}
 
 		auto logoutCallback = [=] {
