66e9f5ba04ba995c07628986385453e94b9c9528
RadRussianRus <radrussianrus@gmail.com>
2022-09-11 06:23:10 +0300

    [Improvement] Do not clear search query after selection in multi-select

--- Telegram/SourceFiles/boxes/peer_list_box.cpp
+++ Telegram/SourceFiles/boxes/peer_list_box.cpp
@@ -224,7 +224,7 @@ void PeerListBox::peerListSetRowChecked(
 		peerListUpdateRow(row);
 
 		// This call deletes row from _searchRows.
-		_select->entity()->clearQuery();
+		//_select->entity()->clearQuery();
 	} else {
 		// The itemRemovedCallback will call changeCheckState() here.
 		_select->entity()->removeItem(row->id());
@@ -240,7 +240,7 @@ void PeerListBox::peerListSetForeignRowChecked(
 		addSelectItem(row, animated);
 
 		// This call deletes row from _searchRows.
-		_select->entity()->clearQuery();
+		//_select->entity()->clearQuery();
 	} else {
 		// The itemRemovedCallback will call changeCheckState() here.
 		_select->entity()->removeItem(row->id());
