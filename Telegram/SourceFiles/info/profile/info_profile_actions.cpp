ecc93b7a30a441952332954091f995770994693c
RadRussianRus <radrussianrus@gmail.com>
2022-09-11 06:16:19 +0300

    [Improvement] Do not hide "open chat" button for own profile

--- Telegram/SourceFiles/info/profile/info_profile_actions.cpp
+++ Telegram/SourceFiles/info/profile/info_profile_actions.cpp
@@ -815,6 +815,7 @@ Ui::MultiSlideTracker DetailsFiller::fillUserButtons(
 			tracker);
 	};
 
+	/*
 	if (user->isSelf()) {
 		auto separator = _wrap->add(object_ptr<Ui::SlideWrap<>>(
 			_wrap,
@@ -830,8 +831,11 @@ Ui::MultiSlideTracker DetailsFiller::fillUserButtons(
 			std::move(tracker).atLeastOneShownValue()
 		);
 	} else {
+	*/
 		addSendMessageButton();
+	/*
 	}
+	*/
 	return tracker;
 }
 
